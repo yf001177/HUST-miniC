b31bfa713a093d775c9d817586e6f9cd3b131db2
cse <321259693@qq.com>
2022-10-25 12:25:19 +0000

    base

---
+++ test_and_answer/init_env.sh
@@ -0,0 +1,11 @@
+# adduser cse
+# 修改ssh允许用户
+
+
+chmod o+w /home/ubuntu/miniC/lab1 -R
+chmod o-rw /home/ubuntu/miniC/lab2 -R
+chmod o-rw /home/ubuntu/miniC/lab3 -R
+chmod o-rw /home/ubuntu/miniC/lab4 -R
+chmod o-rw /home/ubuntu/miniC/lab5 -R
+chmod o-w /home/ubuntu/miniC/answers -R
+chmod o-w /home/ubuntu/miniC/test_and_answer -R
